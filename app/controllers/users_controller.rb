2d158ae77c90cfaf04c0081fa6a7ffa42753f5f2
b25102 <b25102go@aiit.ac.jp>
2025-05-22 04:16:48 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,36 +40,36 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url, status: :see_other
   end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
   # beforeフィルタ
 
   # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      return if logged_in?
+  def logged_in_user
+    return if logged_in?
 
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url, status: :see_other
-    end
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
+  end
 
-    # 正しいユーザーかどうか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
-    end
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
 
-    # 管理者かどうか確認
-    def admin_user
-      redirect_to(root_url, status: :see_other) unless current_user.admin?
-    end
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
 end
