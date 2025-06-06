2665d56360500c30a8e1313d4f4f8a6594265a67
b25102 <b25102go@aiit.ac.jp>
2025-06-06 00:59:47 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-    # render ....
-    #   # => /app/views/static_pages/home.html.erb
+    return unless logged_in?
+
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
-  def help
-    # => /app/views/static_pages/help.html.erb
-    # roots / root / rooting
-    # routes / route /routing
-  end
+  def help; end
 
-  def about
-    # => /app/views/static_pages/about.html.erb
-  end
-  
-  def contact
-    # => /app/views/static_pages/contact.html.erb
-  end
+  def about; end
 
+  def contact; end
 end
